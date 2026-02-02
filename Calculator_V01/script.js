8f1cd47c0e9bab25198144e5509d651515ea77c6
Kuldeep singh <ks25012004.self@gmail.com>
2026-02-02 15:42:15 +0000

    testing

--- Calculator_V01/script.js
+++ Calculator_V01/script.js
@@ -5,10 +5,23 @@ const inputScreen = document
 const resultScreen = document
     .getElementById('result');
 function updateScreen(event) {
-    if (event === 'C') inputScreen.textContent = inputScreen.textContent.slice(0, -1);
-    else if (event === 'clear') inputScreen.textContent = '';
+    if (event === 'C'){ 
+        if(inputScreen.textContent.at(-1) === ' '){
+            inputScreen.textContent = inputScreen.textContent.slice(0, -3);
+        }
+        else{
+            inputScreen.textContent = inputScreen.textContent.slice(0, -1);
+        }
+    }
+    else if (event === 'clear') {
+        inputScreen.textContent = '';
+        resultScreen.textContent = '';
+    }
     else if( (event === '+' || event === '-' || event === '*' || event === '/')  && chechlast() ){
         return;
+    }
+    else if( (event === '+' || event === '-' || event === '*' || event === '/')  && !chechlast() ){
+        inputScreen.textContent += ` ${event} `;
     }
     else
         inputScreen.textContent += event;
@@ -40,10 +53,37 @@ document.onselectstart = (e) => {
   e.preventDefault();
   return false;
 };
-
+let newexpression = [];
 
 function calculate(){
-    console.log('Calculating');
+    const expression = inputScreen.textContent;
+     newexpression = expression.split(' ');
+    while(newexpression.includes('/')){
+        const index = newexpression.indexOf('/');
+        const result = parseFloat(newexpression[index - 1]) / parseFloat(newexpression[index + 1]);
+        newexpression.splice(index - 1, 3, result);
+    }
+    while(newexpression.includes('*')){
+        const index = newexpression.indexOf('*');
+        const result = parseFloat(newexpression[index - 1]) * parseFloat(newexpression[index + 1]);
+        newexpression.splice(index - 1, 3, result);
+    }
+    while(newexpression.includes('+')){
+        const index = newexpression.indexOf('+');
+        const result = parseFloat(newexpression[index - 1]) + parseFloat(newexpression[index + 1]);
+        newexpression.splice(index - 1, 3, result);
+    }
+    while(newexpression.includes('-')){
+        const index = newexpression.indexOf('-');
+        const result = parseFloat(newexpression[index - 1]) - parseFloat(newexpression[index + 1]);
+        newexpression.splice(index - 1, 3, result);
+        if(newexpression.length === 1 || newexpression[0] < 0){
+            newexpression[0] = `${Math.abs(newexpression[0])}-`;
+            console.log('negative');
+        }
+    }
+    if(newexpression.length === 0 && newexpression[0] === NaN) brack;
+    else resultScreen.textContent = newexpression[0];
 }
 function chechlast() {
     const lastchar = inputScreen.textContent.at(-1);
@@ -60,16 +100,16 @@ addEventListener('keydown', (event) => {
     }
      switch (event.key) {
         case '+':
-            updateScreen('+');
+            updateScreen(' + ');
             break;
         case '-':
-            updateScreen('-');
+            updateScreen(' - ');
             break;
         case '*':
-            updateScreen('*');
+            updateScreen(' * ');
             break;
         case '/':
-            updateScreen('/');
+            updateScreen(' / ');
             break;
         case '=':
             calculate();
